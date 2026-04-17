dac098f8d1b82a7c34fe0cb2869f8fcabf4733a5
dixma <dixma@Dix>
2026-04-17 09:45:13 -0500

    aaa

---
+++ ColegioInicialDix/src/main/java/app/controladores/MySQLConexion.java
@@ -0,0 +1,31 @@
+package app.controladores;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConexion {
+	private static final String URL = "jdbc:mysql://localhost:3306/colegio_db";
+	private static final String USUARIO = "root";
+	private static final String CLAVE = "mysql";
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection obtenerConexion() {
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conexion;
+	}
+}
